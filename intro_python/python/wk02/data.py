8f072790fe057a9e57114bb42e11cfefc47d1e2d
Robert Mortimer <robert.mortimer@gmail.com>
2018-10-03 12:41:02 +0100

    Example of asking questions from data file

--- intro_python/python/wk02/data.py
+++ intro_python/python/wk02/data.py
@@ -1,10 +1,57 @@
 import json
+# Load out data
 with open('animal-questions.json', "r") as read_file:
     data = json.load(read_file)
 
-for key in data:
-    print(data[key])
-    record = data[key]
-    if record['ans'] == 'Y':
-      print(record['Q']) 
+# Set my starting question id
+q_id = '16'
+
+# I start without an answer
+ans = 'N'
+
+# Get a Y/N function
+def get_answer (q_text):
+    good_ans = False
+    while good_ans == False:
+        x = str(input(q_text + '? [Y/N]: '))
+        x = x.upper()
+        if x == 'Y':
+            return True
+            good_ans = True
+        elif x == 'N':
+            return False
+            good_ans = False
+        else:
+            print('Enter Y or N')
+
+# Step into questions loop
+while ans == 'N':
+    q_txt = data[q_id]['Q']
+    if get_answer(q_txt):
+        print('YES')
+        # Get next record ID
+        q_id = data[q_id]['Y']
+    else:
+        print('No')
+        # Get next record ID
+        q_id = data[q_id]['N']
+
+    # Is this an answer?
+    if data[q_id]['ans'] == 'Y':
+        print ('Answer is: ' + data[q_id]['Q'])
+        ans ='Y'
+        
+
+    
+
+
+
+
+
+# for key in data:
+#     print(data[key])
+#     record = data[key]
+#     if record['ans'] == 'Y':
+#       print(key)
+#       print(record['Q']) 
 
